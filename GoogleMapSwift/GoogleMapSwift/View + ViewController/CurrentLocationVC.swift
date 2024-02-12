b7848248f99eb39bde8b7a19689b3bfbb08e498c
S. M. Sajid Hasan Shanta <sajidhasan673@gmail.com>
2024-02-12 12:48:10 +0600

    implement CurrentLocation with google map

--- GoogleMapSwift/GoogleMapSwift/View + ViewController/CurrentLocationVC.swift
+++ GoogleMapSwift/GoogleMapSwift/View + ViewController/CurrentLocationVC.swift
@@ -7,14 +7,48 @@
 
 import UIKit
 import GoogleMaps
+import CoreLocation
 
 class CurrentLocationVC: UIViewController {
     @IBOutlet weak var mapView: GMSMapView!
     
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        getUserLocation()
+        setupMap()
     }
+    
+    /// Set up Google Maps view
+    fileprivate func setupMap() {
+        let camera = GMSCameraPosition.camera(withLatitude: 0.0, longitude: 0.0, zoom: 15.0)
+        mapView.camera = camera
+        mapView.isMyLocationEnabled = true
+        mapView.settings.myLocationButton = true
+    }
+    
+    fileprivate func getUserLocation() {
+        // Set up location manager
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
 
+extension CurrentLocationVC: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {        
+        guard let location = locations.last else { return }
+        
+        mapView.animate(to: GMSCameraPosition.camera(withTarget: location.coordinate, zoom: 15.0))
+        
+        mapView.clear()
+        // Creates a marker
+        let marker = GMSMarker()
+        marker.position = location.coordinate
+//        marker.snippet = "BK Banani"
+        marker.map = mapView
+        
+    }
 }

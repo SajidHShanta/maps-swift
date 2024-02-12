eb26cdb02afe4922c4eca7744576ca0ed2ea0c38
S. M. Sajid Hasan Shanta <sajidhasan673@gmail.com>
2024-02-12 14:32:25 +0600

    draw rectangle on map

--- GoogleMapSwift/GoogleMapSwift/View + ViewController/PolygonVC.swift
+++ GoogleMapSwift/GoogleMapSwift/View + ViewController/PolygonVC.swift
@@ -39,5 +39,16 @@ class PolygonVC: UIViewController, GMSMapViewDelegate {
         path.add(CLLocationCoordinate2D(latitude: 23.791137981772383, longitude: 90.40109069620814))
         let polyline = GMSPolyline(path: path)
         polyline.map = mapView
+        
+        // draw rectangle
+        let rectanglePath = GMSMutablePath()
+        rectanglePath.add(CLLocationCoordinate2D(latitude: 23.791119874561975, longitude: 90.40353225006605))
+        rectanglePath.add(CLLocationCoordinate2D(latitude: 23.790923543978405, longitude: 90.403141914213))
+        rectanglePath.add(CLLocationCoordinate2D(latitude: 23.790757201685157, longitude: 90.4041711652695))
+        rectanglePath.add(CLLocationCoordinate2D(latitude: 23.79126800398737, longitude: 90.40445536009842))
+        rectanglePath.add(CLLocationCoordinate2D(latitude: 23.791119874561975, longitude: 90.40353225006605))
+        let rectangle = GMSPolyline(path: rectanglePath)
+        rectangle.strokeColor = .red
+        rectangle.map = mapView
     }
 }

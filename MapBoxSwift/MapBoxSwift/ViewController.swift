ca0a87a01ea6df408a214724e4cbecc19a17aaea
S. M. Sajid Hasan Shanta <sajidhasan673@gmail.com>
2024-02-18 14:44:41 +0600

    dra Marker with mapbox

--- MapBoxSwift/MapBoxSwift/ViewController.swift
+++ MapBoxSwift/MapBoxSwift/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         setupMapBox()
+        draMarker(at: defaultCoordinate)
     }
 
     fileprivate func setupMapBox() {
@@ -25,6 +26,21 @@ class ViewController: UIViewController {
         let defaultCameraOptions = CameraOptions(center: defaultCoordinate, zoom: 15, bearing: 0, pitch: 0)
         mapView.mapboxMap.setCamera(to: defaultCameraOptions)
         view.addSubview(mapView)
+    }
+    
+    fileprivate func draMarker(at someCoordinate: CLLocationCoordinate2D) {
+        // Initialize a point annotation with a CLLocationCoordinate2D
+        var pointAnnotation = PointAnnotation(coordinate: someCoordinate)
+
+        // Make the annotation show a red pin
+        pointAnnotation.image = .init(image: UIImage(named: "dest-pin")!, name: "dest-pin")
+        pointAnnotation.iconSize = 0.04
+
+        // Create the `PointAnnotationManager`, which will be responsible for handling this annotation
+        let pointAnnotationManager = mapView.annotations.makePointAnnotationManager()
+
+        // Add the annotation to the manager in order to render it on the map.
+        pointAnnotationManager.annotations = [pointAnnotation]
     }
 }
 

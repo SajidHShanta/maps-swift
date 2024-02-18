31720ba5225694a501989102a77e84a6bf3238d3
S. M. Sajid Hasan Shanta <sajidhasan673@gmail.com>
2024-02-18 14:43:23 +0600

    setup map with mapbox

--- MapBoxSwift/MapBoxSwift/ViewController.swift
+++ MapBoxSwift/MapBoxSwift/ViewController.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
+import MapboxMaps
 
 class ViewController: UIViewController {
-
+    
+    var mapView: MapView!
+    let defaultCoordinate = CLLocationCoordinate2D(latitude: 23.7925, longitude: 90.4057)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupMapBox()
     }
 
-
+    fileprivate func setupMapBox() {
+        mapView = MapView(frame: view.bounds)
+        let defaultCameraOptions = CameraOptions(center: defaultCoordinate, zoom: 15, bearing: 0, pitch: 0)
+        mapView.mapboxMap.setCamera(to: defaultCameraOptions)
+        view.addSubview(mapView)
+    }
 }
 
